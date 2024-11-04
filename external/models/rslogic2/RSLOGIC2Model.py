75f966ae8ca7f329fd707578ea72117e378b5d07
salvatore-bufi <bufi.salvatore.94@gmail.com>
2024-11-04 18:44:19 +0100

    new model: RSLOGIC update: try with othe rdisjunction operator

--- external/models/rslogic2/RSLOGIC2Model.py
+++ external/models/rslogic2/RSLOGIC2Model.py
@@ -1,8 +1,7 @@
 import torch
 import numpy as np
-import torch.nn.functional as F
-from abc import ABC
 import random
+from abc import ABC
 
 class RSLOGIC2Model(torch.nn.Module, ABC):
     def __init__(self,
@@ -18,12 +17,9 @@ class RSLOGIC2Model(torch.nn.Module, ABC):
         super().__init__()
 
         # Set seed for reproducibility
-        random.seed(random_seed)
-        np.random.seed(random_seed)
         torch.manual_seed(random_seed)
-        torch.cuda.manual_seed(random_seed)
-        torch.cuda.manual_seed_all(random_seed)
-        torch.backends.cudnn.deterministic = True
+        np.random.seed(random_seed)
+        random.seed(random_seed)
 
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -32,97 +28,100 @@ class RSLOGIC2Model(torch.nn.Module, ABC):
         self.embed_k = embed_k
         self.learning_rate = learning_rate
         self.l_w = l_w
+        self.logic_w = 0.05
 
-        # User and item embeddings
+        # Embeddings for users and items
         self.Gu = torch.nn.Embedding(self.num_users, self.embed_k, device=self.device)
-        torch.nn.init.xavier_uniform_(self.Gu.weight)
-
         self.Gi = torch.nn.Embedding(self.num_items, self.embed_k, device=self.device)
+        torch.nn.init.xavier_uniform_(self.Gu.weight)
         torch.nn.init.xavier_uniform_(self.Gi.weight)
 
+        # Convert ui to tensor on device
+        self.ui = torch.tensor(ui, device=self.device, requires_grad=False)
+        self.user_history = {int(u): self.ui[:, self.ui[0, :] == u][1, :] for u in self.ui[0, :].unique()}
 
-
-
-        self.ui = torch.tensor(ui, device=self.device)
-
+        # Optimizer
         self.optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
 
-
     def like_history(self, u: torch.Tensor, i: torch.Tensor) -> torch.Tensor:
-        # calculate the score of each u-i pair in the history
-        return torch.sigmoid(torch.matmul(u.to(self.device), torch.transpose(i.to(self.device), 0, 1))).unsqueeze(0)
+        return torch.sigmoid(torch.matmul(u, i.T)).unsqueeze(0)
 
     def antecedent(self, x: torch.Tensor) -> torch.Tensor:
-        # from implication to dnf form
         return 1. - x
 
-    def disjunction(self, selector: torch.Tensor, selected: torch.Tensor, epsilon: float = 1e-40) -> torch.Tensor:
+    def disjunction_nips(self, selector: torch.Tensor, selected: torch.Tensor, epsilon: float = 1e-40) -> torch.Tensor:
         expr = torch.log(1. - selector * selected + epsilon)
         log_sum = torch.sum(expr, dim=1)
-        # log_sum = torch.sum(expr)
         return 1. - (-1.0 / (-1.0 + log_sum))
+
+    def disjunction_godel(self, selected: torch.Tensor) -> torch.Tensor:
+        return torch.max(selected)
+
+
+
+    def disjunction(self, selected: torch.Tensor, f = '1') -> torch.Tensor:
+        if f == '1':
+            return self.disjunction_godel(selected)
+        else:
+            return self.disjunction_nips(1.0, selected)
 
 
 
     def premise_user_history(self, u: int) -> torch.Tensor:
-        ## calculate the score of the part before implication (i.e. history)
-        user_history = self.ui[:, self.ui[0, :] == u][1, :] ## torch tensor of lenght == number_of_items the user interacted
+        user_history = self.user_history[u]
         gu = self.Gu.weight[u]
-        gi = self.Gi.weight[user_history]
+        gi = self.Gi(user_history)
         rule_antecedent = self.antecedent(self.like_history(gu, gi))
-        return self.disjunction(selector=1.0, selected=rule_antecedent)
-
+        # return self.disjunction(selector=1.0, selected=rule_antecedent)
+        # return self.disjunction(rule_antecedent)
+        return self.disjunction(rule_antecedent, f='1')
     def calculate_batch_premise(self, users: np.array):
-        ''' user: np.array of indices of users'''
-        u_premis_list = []
-        for u in users:
-            premise = self.premise_user_history(u)
-            u_premis_list.append(premise)
+        u_premis_list = [self.premise_user_history(u).unsqueeze(0) for u in users]
         return torch.cat(u_premis_list, dim=0)
 
     def forward_logic(self, inputs, **kwargs):
-        users, items, scores = inputs
-        users_premises = self.calculate_batch_premise(users).unsqueeze(1)
+        users_premises, scores = inputs
+        users_premises = users_premises.unsqueeze(1)
         ui = torch.sigmoid(scores).unsqueeze(1)
-        xui = self.disjunction(selector=1.0, selected=torch.cat((ui, users_premises), dim=1))
+        # xui = self.disjunction(selector=1.0, selected=torch.cat((ui, users_premises), dim=1))
+        xui = self.disjunction(torch.cat((ui, users_premises), dim=1), f='1')
         return xui
 
     def forward(self, inputs, **kwargs):
         users, items = inputs
-        gamma_u = torch.squeeze(self.Gu.weight[users]).to(self.device)
-        gamma_i = torch.squeeze(self.Gi.weight[items]).to(self.device)
-
-        xui = torch.sum(gamma_u * gamma_i, 1)
-
+        gamma_u = self.Gu.weight[users]
+        gamma_i = self.Gi.weight[items]
+        xui = torch.sum(gamma_u * gamma_i, dim=1)
         return xui, gamma_u, gamma_i
 
     def predict(self, start, stop, **kwargs):
-        return torch.matmul(self.Gu.weight[start:stop].to(self.device),
-                            torch.transpose(self.Gi.weight.to(self.device), 0, 1))
+        return torch.matmul(self.Gu.weight[start:stop], self.Gi.weight.T)
 
     def train_step(self, batch):
         user, pos, neg = batch
         xu_pos, gamma_u, gamma_i_pos = self.forward(inputs=(user[:, 0], pos[:, 0]))
         xu_neg, _, gamma_i_neg = self.forward(inputs=(user[:, 0], neg[:, 0]))
 
-        logic_score_pos = self.forward_logic(inputs=(user[:, 0], pos[:, 0], xu_pos))
-        logic_score_neg = self.forward_logic(inputs=(user[:, 0], neg[:, 0], xu_neg))
 
         loss = -torch.mean(torch.nn.functional.logsigmoid(xu_pos - xu_neg))
 
+        premise = self.calculate_batch_premise(user[:, 0])
+        logic_score_pos = self.forward_logic(inputs=(premise, xu_pos))
+        logic_score_neg = self.forward_logic(inputs=(premise, xu_neg))
         logic_loss = -torch.mean(torch.nn.functional.logsigmoid(logic_score_pos - logic_score_neg))
-        loss += logic_loss
+        # print(f"\nBPR LOSS: {loss} \t LOGIC LOSS: {logic_loss}")
+        loss += self.logic_w * logic_loss
 
-        reg_loss = self.l_w * (1 / 2) * (gamma_u.norm(2).pow(2) +
-                                         gamma_i_pos.norm(2).pow(2) +
-                                         gamma_i_neg.norm(2).pow(2)) / user.shape[0]
+        reg_loss = self.l_w * 0.5 * (gamma_u.norm(2).pow(2) +
+                                     gamma_i_pos.norm(2).pow(2) +
+                                     gamma_i_neg.norm(2).pow(2)) / user.shape[0]
         loss += reg_loss
 
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
 
-        return loss.detach().cpu().numpy()
+        return loss.detach().cpu().item()
 
     def get_top_k(self, preds, train_mask, k=100):
         return torch.topk(torch.where(torch.tensor(train_mask).to(self.device), preds.to(self.device),
